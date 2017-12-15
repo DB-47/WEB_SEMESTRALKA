f7baebe044f9dfec71f49e79f1746c585b6d0305
DB-47 <34443035+DB-47@users.noreply.github.com>
2017-12-15 13:28:56 +0100

    Modely obohaceny o ENUMy

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,8 @@
 class Course < ApplicationRecord
+
+  extend Enumerize
+
+  enumerize :language, in: [:czech, :english], default: :czech
+  enumerize :study_type, in: [:full_time, :part_time], default: :full_time
+
 end
--- db/migrate/20171215120035_create_students.rb
+++ db/migrate/20171215120035_create_students.rb
@@ -4,6 +4,7 @@ class CreateStudents < ActiveRecord::Migration[5.1]
       t.string :first_name
       t.string :last_name
       t.string :email
+      t.string :study_type
 
       t.timestamps
     end
