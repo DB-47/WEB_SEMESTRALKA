c6d2b07533f7a024f53a355181118ed9feca51dd
DB-47 <34443035+DB-47@users.noreply.github.com>
2017-12-16 17:38:21 +0100

    Připraveny opravené kontrolery, migrace DB a modely. FIX

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171216143409) do
+ActiveRecord::Schema.define(version: 20171216160810) do
 
   create_table "buildings", force: :cascade do |t|
     t.string "title"
